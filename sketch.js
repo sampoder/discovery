147eb8d2f99dd3d53c2ac62c12ae6d10aa3a74e6
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-09-18 18:20:08 +0800

    Sin Updates

--- sketch.js
+++ sketch.js
@@ -4,19 +4,38 @@ function setup() {
 
 var degree = 0;
 
+let externalMultiplier = 1;
+
+let internalMultiplier = 1;
+
+let externalAddition = 0;
+
+let internalAddition = 0;
+
 function draw() {
   setTimeout(makeArt, 2000);
   degree = degree + 1;
 }
 
+var pusher = new Pusher("9e9ba095c4dd756509de", {
+  cluster: "ap1",
+});
+
+var channel = pusher.subscribe("main-channel");
+channel.bind("sine-update", function (data) {
+  externalMultiplier = parseFloat(data.externalMultiplier);
+  internalMultiplier = parseFloat(data.internalMultiplier);
+  externalAddition = parseFloat(data.externalAddition);
+  internalAddition = parseFloat(data.internalAddition);
+});
+
 function makeArt() {
   background("Black");
-  console.log(degree);
-  var step = 40 + (Math.sin((degree * Math.PI) / 180)) * 20;
+  var step = 40 + (Math.sin((internalMultiplier * degree * Math.PI) / 180)) * 20;
   for (var x = 0; x < innerWidth; x = x + step) {
     for (var y = 0; y < innerHeight; y = y + step) {
       strokeWeight(3);
-      stroke('rgb(0,255,0)')
+      stroke("rgb(0,255,0)");
       line(x, y, x + 120, y + 120);
     }
   }
