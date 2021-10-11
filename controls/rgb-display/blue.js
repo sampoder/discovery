a07ee944d25d91095e6d4b8de72ca48f0fcca6b9
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-10-11 17:51:00 +0800

    RGB!

---
+++ controls/rgb-display/blue.js
@@ -0,0 +1,19 @@
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [255/2],
+    connect: 'lower',
+    range: {
+        'min': 0,
+        'max': 255
+    }
+});
+
+slider.noUiSlider.on('update', function (values, handle) {
+    console.log(values[handle])
+    var all = document.getElementsByClassName('noUi-connect');
+    for (var i = 0; i < all.length; i++) {
+      all[i].style.background = `rgb(0, 0, ${values[handle]})`;
+    }
+    fetch(`/api/events/rgb-update?colour=blue&value=${values[handle]}`)
+});
